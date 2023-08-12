87851e52b77d1fbfade9cf92831638335e350c9e
Brian Chew <brian@bchewy.com>
2023-08-12 09:42:47 +0800

    slash comands new interactions, not old discord

--- bot.py
+++ bot.py
@@ -1,27 +1,48 @@
 import os
 from dotenv import load_dotenv
-import discord
-from discord.ext import commands
+# import discord
+# from discord.ext import commands
+from interactions import Client, Intents, listen, slash_command, SlashContext
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-# print(BOT_TOKEN)
 
-intents = discord.Intents.default()
-intents.messages = True
-intents.guilds = True
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.event
+bot = Client(intents=Intents.DEFAULT)
+@listen()  # this decorator tells snek that it needs to listen for the corresponding event, and run this coroutine
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    # This event is called when the bot is ready to respond to commands
+    print("Ready")
+    print(f"This bot is owned by {bot.owner}")
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Hello there!")
+@listen()
+async def on_message_create(event):
+    # This event is called when a message is sent in a channel the bot can see
+    print(f"message received: {event.message.content}")
+    print(event)
+    print(event.message)
+    print(event.message.content)
 
-# Run the bot.
-bot.activity = discord.Game(name="with the API")
-bot.run(BOT_TOKEN)
+@slash_command(name="my_command", description="My first command :)")
+async def my_command_function(ctx: SlashContext):
+    await ctx.send("Hello World")
+
+bot.start(BOT_TOKEN)
+
+# intents = discord.Intents.default()
+# intents.messages = True
+# intents.guilds = True
+# intents.message_content = True
+
+# bot = commands.Bot(command_prefix='!', intents=intents)
+# slash = SlashCommand(bot, sync_commands=True)  # Initializes the slash command system
+
+# @bot.event
+# async def on_ready():
+#     print(f"We have logged in as {bot.user}")
+
+# @slash.slash(name="hello", description="Say hello to the bot!")  # Define the slash command
+# async def _hello(ctx: SlashContext):
+#     await ctx.send(content="Hello there!")
+
+# bot.activity = discord.Game(name="with the API")
+# bot.run(BOT_TOKEN)
