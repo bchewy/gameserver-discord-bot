badbcb34bf0920e69bb18405cbf06383a654620f
Brian Chew <brian@bchewy.com>
2023-08-12 14:57:08 +0800

    update

--- main.tf
+++ main.tf
@@ -116,9 +116,6 @@ resource "aws_instance" "ec2" {
               #!/bin/bash             
               sudo dpkg --add-architecture i386
               sudo apt update
-
-              sudo yum install -y awslogs
-
               sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 -y
               
               echo sleeping
